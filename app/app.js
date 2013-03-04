a5741fc7538a752dc0d73ae3bb19aa5ab41bb451
Mark Seaman <mark.seaman@shrinking-world.com>
2013-03-04 15:30:05 -0700

    Initial application

---
+++ app/app.js
@@ -0,0 +1,65 @@
+// System requirements
+var express    = require('express');
+var app        = express();
+var http       = require('http');
+var server     = http.createServer(app);
+var files      = require('./files');
+
+
+// Setup environment
+app.set('view engine', 'jade');
+app.set('view options', { layout: true });
+app.set('views',        __dirname+'/views');
+
+app.use(express.bodyParser()); // Automatically parses form data
+
+// Static files
+app.get('/views/*?:file?', function(req, res){
+    res.sendfile ('views/'+req.params.file);
+});
+
+// List view
+app.get('/', function(req, res){
+    files.list ('../doc', function (data) { res.render ('list', data) });
+});
+
+// New view
+app.get('/new', function(req, res) {
+    res.render ('edit', {path:'', text:''} ); 
+});
+
+// Doc pages
+app.get('/:doc', function(req, res){
+    doc = req.params.doc;
+    files.wiki(doc, function(stdout) {
+        res.render('doc',{path:doc, text:stdout});    
+    });
+});
+
+// Edit view
+app.get('/:doc/edit', function(req, res) {
+    path = req.params.doc;
+    files.read ('../doc/'+path, function (data) { 
+        res.render ('edit', {path:path, text:data.data} ); 
+    });
+});
+
+// Save view
+app.post('/save', function(req, res){
+    path = req.body.path;
+    text = req.body.text.replace(/\r/gm,'');
+    if (req.param('cancel')) return res.redirect ('/'+path); 
+    files.write ('../doc/'+path, text, function () {
+        res.redirect ('/'+path); 
+    });
+});
+
+// Home page
+app.get('*', function(req, res){
+    res.redirect('/Home');    
+});
+
+// Listen on 8080
+var port = 8080;
+server.listen(port);
+console.log('Listening on port ' + port);
---
+++ app/files.js
@@ -0,0 +1,49 @@
+var fs      = require('fs');
+var exec       = require('child_process').exec;
+
+
+// List the files and perform an action
+var list_files = function (path, action) {
+    fs.readdir(path, function(err, files) {
+        if (err) files = ['No directory'];
+        action({ files:files });
+    });
+}
+
+// Read the file and perform an action
+var read_file = function (path, action) {
+    fs.readFile(path, 'utf8', function(err, data) {
+        if (err) data = 'No file found';
+        action({ id:path, data:data });
+    });
+}
+
+// Read the file and perform an action
+var write_file = function (path, text, action) {
+    var stream = fs.createWriteStream(path);
+    stream.once('open', function(fd) {
+        stream.write(text+"\n");
+        stream.end();
+        action();
+    });
+}
+
+// Execute a command and act on the output
+var execute_file = function(command, action) {
+    exec(command, function(error,stdout) {
+        action(stdout);    
+    });
+}
+
+// Format a wiki page
+var wiki_file = function(doc, action) {
+    execute_file('wiki-html-content ../doc/'+doc, function(stdout) {
+        action(stdout);
+    });
+}
+
+exports.list    = list_files;
+exports.read    = read_file;
+exports.write   = write_file;
+exports.execute = execute_file;
+exports.wiki    = wiki_file;
