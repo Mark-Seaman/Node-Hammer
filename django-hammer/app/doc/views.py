ac4ef35fdb27a6898705a680b6aa750d90a3631d
Mark Seaman <mark.seaman@shrinking-world.com>
2013-03-22 14:36:10 -0600

    Working viewer with navigation

--- django-hammer/app/doc/views.py
+++ django-hammer/app/doc/views.py
@@ -1,6 +1,7 @@
 from django.http        import HttpResponse
 from django.shortcuts   import render
 from os                 import listdir
+from subprocess         import Popen,PIPE
 
 def home(request):
     return render (request, 'index.html')
@@ -10,4 +11,13 @@ def list(request):
     return render (request, 'list.html', { 'directory': d })
 
 def doc(request,title):
-    return render(request, 'doc.html', { 'title': title})
+    return render(request, 'doc.html', { 'title': title, 'text': read_doc(title)})
+
+
+# Run the command as a process and capture stdout & print it
+def do_command(cmd):
+    return  Popen(cmd.split(),stdout=PIPE).stdout.read()
+
+# Run the echo command
+def read_doc(title):
+    return do_command('wiki-html-content ../doc/'+title)
