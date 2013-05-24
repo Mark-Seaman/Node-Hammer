afac40c115b5e9a2f1eba364aa279d1888a75f37
Mark Seaman <mark.seaman@shrinking-world.com>
2013-05-24 13:40:23 -0600

    Remove django-hammer from this project

--- django-hammer/old/doc/views.py
+++
@@ -1,8 +0,0 @@
-from django.http        import HttpResponse
-from django.shortcuts   import render
-from os                 import listdir
-
-def home(request):
-    d = listdir('/home/seaman/Projects/hammer/django-hammer/doc')
-    return render (request, 'list.html', { 'directory': d })
-
