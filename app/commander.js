ca2de786968b8f21db0bf33b11c013b4f557b417
Mark Seaman <mark.seaman@shrinking-world.com>
2013-05-24 16:00:06 -0600

    Create basic commander

---
+++ app/commander.js
@@ -0,0 +1,79 @@
+// System requirements
+var express    = require('express');
+var app        = express();
+var http       = require('http');
+var server     = http.createServer(app);
+var files      = require('./files');
+
+
+// Setup environment
+app.set('view engine', 'jade');
+app.set('view options', { layout: true });
+app.set('views',        __dirname+'/views');
+
+app.use(express.bodyParser()); // Automatically parses form data
+
+//-----------------------------------------------------------------------------
+// Static files
+app.get('/favicon.ico', function(req, res){
+    res.sendfile ('views/favicon.ico');
+});
+
+app.get('/views/*?:file?', function(req, res){
+    res.sendfile ('views/'+req.params.file);
+});
+
+// List view
+app.get('/:app/', function(req, res){
+    app = req.params.app;
+    files.list (app, function (data) { res.render ('list', data) });
+});
+
+// New view
+app.get('/:app/new', function(req, res) {
+    app = req.params.app;
+    res.render ('edit', {app:app, path:'', text:''} ); 
+});
+
+
+// Edit view
+app.get('/:app/*:doc?/edit', function(req, res) {
+    app = req.params.app;
+    doc = req.params.doc;
+    files.read (app, doc, function (text) { 
+        res.render ('edit', {app:app, doc:doc, text:text} ); 
+    });
+});
+
+// Save view
+app.post('/:app/edit', function(req, res){
+    app = req.params.app;
+    doc = req.body.doc;
+    text = req.body.text.replace(/\r/gm,'');
+    if (req.param('cancel')) return res.redirect ('/'+app+'/'+doc); 
+    files.write (app, doc, text, function () {
+        res.redirect ('/'+app+'/'+doc); 
+    });
+});
+
+// Doc pages
+app.get('/:app/*:doc?', function(req, res){
+    doc = req.params.doc;
+    app = req.params.app;
+    files.format(app, doc, 
+                 function(text) { res.render('show',{app:app, path:doc, text:text}) },   
+                 function()     { res.send ('Error')}
+                )
+
+});
+
+// Home page
+app.get('*', function(req, res){
+    console.log("Page:"+req.url)
+    res.redirect ('/cmd/Home')
+});
+
+// Listen on 8080
+var port = 8080;
+server.listen(port);
+console.log('Listening on port ' + port);
--- app/files.js
+++ app/files.js
@@ -40,7 +40,23 @@ var format_file = function(app, doc, show, create) {
     })
 }
 
+// Execute a script and return the result
+var execute_file = function(app, doc, show, create) {
+    path = '../doc/'+app+'/'+doc
+    fs.exists(path, function(exists) {
+        if (exists) { 
+            exec('hammer-show '+path, function(error,stdout) { 
+                show(stdout) 
+            })
+        }
+        else {
+           create()
+        }
+    })
+}
+
 exports.list    = list_files;
 exports.read    = read_file;
 exports.write   = write_file;
+exports.execute = execute_file;
 exports.format  = format_file;
