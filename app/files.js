37543d341b3230ed75c5fc917adf69be95a03c89
Mark Seaman <mark.seaman@shrinking-world.com>
2013-06-15 11:39:51 -0600

    Fix edit operation

--- app/files.js
+++ app/files.js
@@ -4,18 +4,16 @@ var exec  = require('child_process').exec;
 
 // Read the file and perform an action
 var read_file = function (doc, action) {
-    exec('hammer-read $p/doc/'+doc, function(error,stdout) {
+    exec('hammer-read '+doc, function(error,stdout) {
         action(stdout);
     });   
 }
 
 // Read the file and perform an action
 var write_file = function (doc, text, action) {
-    p = exec('hammer-edit ../doc/'+doc, function(error,stdout) {
+    exec('echo "'+text+'"|hammer-edit '+doc, function(error,stdout) {
         action(stdout);
     })
-    p.stdin.write(text);
-    p.stdin.end();
 }
 
 // Format a wiki page
